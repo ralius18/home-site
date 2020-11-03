7cdda428b667434443905ca6eacd4fa9e3f55cbe
ralius18 <brayds97@gmail.com>
2020-11-03 21:56:25 +1300

    More work on sidenav

--- resources/main.js
+++ resources/main.js
@@ -3,3 +3,13 @@ function toggleDark() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+function mouseOverSVG(id) {
+  id = id + '-fill'
+  $('#' + id).attr('fill', 'rgb(170, 0, 0)')
+}
+
+function mouseOutSVG(id) {
+  id = id + '-fill'
+  $('#' + id).attr('fill', 'rgb(189, 189, 189)')
+}
