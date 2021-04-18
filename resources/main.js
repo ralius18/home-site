947f8515ae40fdbfad1938e613bd0ea0fd3bbc3e
Brad Stone <brayds97@gmail.com>
2021-04-19 09:24:25 +1200

    Downsize images, dark mode toggling, art zoom

--- resources/main.js
+++ resources/main.js
@@ -1,3 +1,20 @@
+function artPageLoad() {
+  var imgArr = document.getElementsByClassName('art-img')
+  var closeSpan = document.getElementById('span-close')
+  var modal = document.getElementById('modal')
+
+  checkDarkCookie()
+
+  for (let img of imgArr) {
+    img.onclick = function () {
+      expandPicture(img.id)
+    }
+  };
+  closeSpan.onclick = modal.onclick = function () {
+    modal.style.display = 'none'
+  }
+}
+
 function checkDarkCookie(){
   if (getCookie('darkmode') === 'false') {
     toggleDark(false)
@@ -7,11 +24,15 @@ function checkDarkCookie(){
 function toggleDark(changeCookie = true) {
   var element = document.body
   var setDark = getCookie('darkmode') === 'true' ? 'false' : 'true'
+  element.style.setProperty('transition', 'none')
+  element.style.setProperty('-webkit-transition', 'none')
   element.classList.toggle("dark-mode")
 
   if (changeCookie) {
     setCookie('darkmode', setDark, 30)
   }
+  element.style.setProperty('transition', 'all 0.5s ease-in-out')
+  element.style.setProperty('-webkit-transition', 'all 0.5s ease-in-out')
 }
 
 function mouseOverSVG(id) {
@@ -45,4 +66,17 @@ function getCookie(name) {
 }
 function eraseCookie(name) {
   document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function expandPicture(id) {
+  var img = $('#' + id)
+  var modal = document.getElementById('modal')
+  var modalImg = document.getElementById('modal-img')
+  var captionEl = document.getElementById('caption-text')
+  var captionText = img.parent('td').text()
+
+  captionText.replaceAll(/\\n/g, '<br>')
+  modal.style.display = 'block'
+  modalImg.src = img.attr('src')
+  captionEl.innerHTML = captionText
 }
